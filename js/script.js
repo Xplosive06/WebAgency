260b0c8d60b532d3734028c844f8033b4e245a36
Mike <nottamike92@gmail.com>
2019-04-19 11:10:52 +0200

    add navbar burger on smaller screen size

--- js/script.js
+++ js/script.js
@@ -29,3 +29,12 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
 }
+
+function navbarFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
